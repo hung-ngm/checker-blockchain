ae6def8fc4ce67301fdbce29c01a4b60bf901fd5
minhhung123 <prohungnguyenminh@gmail.com>
2022-03-09 17:53:48 +1100

    Add message handler for createGame

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,8 +2,10 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	rules "github.com/minhhung123/checkers/x/checkers/rules"
 	"github.com/minhhung123/checkers/x/checkers/types"
 )
 
@@ -11,7 +13,27 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
-	_ = ctx
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+		panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Game: rules.New().String(),
+		Red: msg.Red,
+		Black: msg.Black,
+	}
 
-	return &types.MsgCreateGameResponse{}, nil
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	return &types.MsgCreateGameResponse{
+		IdValue: newIndex,
+	}, nil
 }
